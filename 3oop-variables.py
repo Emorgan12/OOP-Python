dd99c101bfee6879450ad0d96a60e0a36ccf677b
Syed Murad <89536015+syedmurad1@users.noreply.github.com>
2021-11-25 08:58:19 +0000

    Add files via upload

---
+++ 3oop-variables.py
@@ -0,0 +1,9 @@
+character_name = "Tom"
+character_age = "90"
+
+print("There once was a human named " + character_name)
+print("He was " + character_age + " years old")
+print("He really liked the name " + character_name)
+print("But didn't like being " + character_age)
+print(character_name+" wants to build time machine")
+
