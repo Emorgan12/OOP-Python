16598622a5581d5b2fd4bfbde67fb9f0fda3012f
Syed Murad <murad6000@yahoo.com>
2021-11-26 15:04:36 +0000

    https://github.com/syedmurad1/OOP-Python.git

---
+++ 5oop-str.py
@@ -0,0 +1,4 @@
+num =5
+print(num)
+print(str(num)+ " is my num")
+# print(num + "my num") - will not work 
---
+++ 6oop-pow-max-min.py
@@ -0,0 +1,7 @@
+
+num =-5
+print(abs(num))
+num =5
+print(pow(3,2)) 
+print(max(3,2)) 
+print(min(3,2)) 
