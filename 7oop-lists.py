acce1f0cf8e6a6801d73ae71de25b8bf573ff830
Syed Murad <murad6000@yahoo.com>
2021-11-26 15:24:25 +0000

    https://github.com/syedmurad1/OOP-Python.git

---
+++ 7oop-lists.py
@@ -0,0 +1,8 @@
+numbers = [4, 8, "fifteen", 16, 23, 42.0]
+#       indexes  0  1       2      3   4   5
+
+print(numbers)
+print(numbers[2])
+print(numbers[-1])
+print(numbers[1:]) #after 0
+print(numbers[1:3]) # 1 and 3 index
