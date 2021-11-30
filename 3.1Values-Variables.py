41b768ee34d69fae1026a326a44d5ec6c6233f82
Syed Murad <murad6000@yahoo.com>
2021-11-30 13:04:58 +0000

    commit

--- 3.1Values-Variables.py
+++ 3.1Values-Variables.py
@@ -1,4 +1,17 @@
-x, y, z = "Orange", "Banana", "Cherry"
+x, y, z = "Orange", "Banana", "Cherry" # Many Values to Multiple Variables
+print(x)
+print(y)
+print(z)
+
+
+x = y = z = "Mango" # One Value to Multiple Variables
+print(x)
+print(y)
+print(z)
+
+
+fruits = ["apple", "banana", "cherry"] # Unpack a Collection. for more see 7oop-lists.py
+x, y, z = fruits
 print(x)
 print(y)
 print(z)
