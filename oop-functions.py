5f85698ae739ae4c0f14d7d2578f33ef2f258d05
Syed Murad <murad6000@yahoo.com>
2021-12-02 08:38:27 +0000

    commit

--- oop-functions.py
+++ oop-functions.py
@@ -3,3 +3,24 @@ def sayHi():  # def is for Functions
 
 sayHi() # to call
 
+print("--------------------------------------------------------")
+
+def sayHi(name):
+    print("Hello " + name)
+
+sayHi("Syed")
+
+print("--------------------------------------------------------")
+
+def sayHi(name, age):
+    print("Hello " + name + " you are " + age)
+
+sayHi("Tom", "24")
+sayHi("Syed", "99")
+
+print("--------------------------------------------------------")
+
+def sayHi(name,age):
+    print("Hello " + name + " you are " + str(age))
+
+sayHi("Syed", 94)
