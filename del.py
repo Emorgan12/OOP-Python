9b8006fa98431c989cbc710b398b41f9133cb559
Syed Murad <murad6000@yahoo.com>
2021-12-03 14:25:33 +0000

    commit

--- del.py
+++ del.py
@@ -1,3 +1,12 @@
 print("try")
 
+class Book:
+  def __init__(self, title, author, numPages):
+      self.title = title
+      self.author = author
+      self.numPages = numPages
 
+class Question:
+     def __init__(self, prompt, answer):
+          self.prompt = prompt
+          self.answer = answer
