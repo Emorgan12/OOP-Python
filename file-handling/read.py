3dc1c0bf573d03b32c9c266fc555a8fe4187c57c
Syed Murad <murad6000@yahoo.com>
2022-01-26 23:55:23 +0000

    commit

--- file-handling/read.py
+++ file-handling/read.py
@@ -1,3 +1,10 @@
-f = open("C:\Users\ITC-LAB-T\Desktop\OOP-Python\file-handling\syed.txt", "rt")
-print(f.read())
+t = open(r"C:\Users\ITC-LAB-T\Desktop\OOP-Python\file-handling\syed.txt", "rt")       #(r"syed.txt", "rt")     
+#print(t.read())
+#print(t.read(7))
+#print(t.readline())
+
+# for x in t:
+#   print(x)
+
+
 
