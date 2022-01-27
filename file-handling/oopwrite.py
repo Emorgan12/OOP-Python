6202addfc1466f94eca6e0c3174d3f282038e751
Syed Murad <murad6000@yahoo.com>
2022-01-27 00:12:51 +0000

    commit

--- file-handling/oopwrite.py
+++ file-handling/oopwrite.py
@@ -1,10 +1,17 @@
 # "a" - Append - will append to the end of the file
-ow = open(r"abc.txt", "a")
+ow = open("abc.txt", "a")
 ow.write("I can write whatever I want!")
 ow.close()
 
+# "w" - Write - will overwrite any existing content
+# ow = open("abc.txt", "w")
+# ow.write("I can write whatever I want! W.")
+# ow.close()
+
 #open and read the file after the appending:
-ow = open(r"abc.txt", "r")
+ow = open("abc.txt", "r")
 print(ow.read())
 
 ow.close()
+
+
