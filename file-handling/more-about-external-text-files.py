d449feea2bffa8f39a39e61c87cf5804866ed80a
Syed Murad <murad6000@yahoo.com>
2022-01-31 17:55:09 +0000

    commit

--- file-handling/more-about-external-text-files.py
+++ file-handling/more-about-external-text-files.py
@@ -3,3 +3,11 @@
 # line.split()
 
 
+f = open("abc.txt", "r")
+print(f.readline())  # the first line of the file
+
+f = open("abc.txt", "r")
+print(f.readline(3)) # Return only the three first bytes from the first line:
+print(f.readlines()) # Return all lines in the file, as a list where each line is an item in the list object
+print(f.readlines(3)) # Do not return the next line if the total number of returned bytes are more than 3
+
