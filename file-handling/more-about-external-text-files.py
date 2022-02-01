218ade92558add917ac45882215bce40cccaa957
Syed Murad <murad6000@yahoo.com>
2022-02-01 09:47:46 +0000

    commit

--- file-handling/more-about-external-text-files.py
+++ file-handling/more-about-external-text-files.py
@@ -11,3 +11,7 @@ print(f.readline(3)) # Return only the three first bytes from the first line:
 print(f.readlines()) # Return all lines in the file, as a list where each line is an item in the list object
 print(f.readlines(3)) # Do not return the next line if the total number of returned bytes are more than 3
 
+
+txt = "welcome to the jungle"
+x = txt.split()
+print(x)
