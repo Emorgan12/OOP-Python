b6f29b2c487f9e372b66e21974f5aff29ce0e1d3
Syed Murad <murad6000@yahoo.com>
2022-02-01 10:01:36 +0000

    commit

--- file-handling/more-about-external-text-files.py
+++ file-handling/more-about-external-text-files.py
@@ -12,6 +12,19 @@ print(f.readlines()) # Return all lines in the file, as a list where each line i
 print(f.readlines(3)) # Do not return the next line if the total number of returned bytes are more than 3
 
 
-txt = "welcome to the jungle"
-x = txt.split()
+txt = "welcome to the class"
+x = txt.split() # Split a string into a list where each word is a list item
+print(x)
+
+
+txt = "welcome,to the class"
+x = txt.split(",")
+print(x)
+
+txt = "welcome#to#the#class"
+x = txt.split("#")
+print(x)
+
+txt = "welcome#to#the#class"
+x = txt.split("#", 1)
 print(x)
