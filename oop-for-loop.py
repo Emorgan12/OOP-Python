2cec93e9526b49fd26f536442584a7d5470f7cfe
Syed Murad <murad6000@yahoo.com>
2022-04-28 11:10:33 +0100

    commit

--- oop-for-loop.py
+++ oop-for-loop.py
@@ -9,6 +9,16 @@ for fr in friends:
 print("--------------------------------------------------------")
 for num in range(4):
     print(num)
+print("--------------------------------------------------------")
+for num in range(1,4):
+    print(num)
+
+print("--------------------------------------------------------")
+
+number=int(input("enter an integer: "))
+for num in range(1,4):
+    cal=number* num
+    print(number, "X", num, "=", cal)
 
 print("--------------------------------------------------------")
 fr1=["Tom","Ali","M"]
