571b21297212a37aee9450a1ac0e25d4631f2a8c
Syed Murad <murad6000@yahoo.com>
2023-02-21 08:11:15 +0000

    commit

---
+++ Ex/Ex-T/ex02.py
@@ -0,0 +1,12 @@
+# Ref In "read me" file and only for educational purpose.
+
+# Ref In "read me" file and only for educational purpose.
+
+# 1. Write a Python program to find those numbers which are divisible by 7 and multiples of 5, between 1000 and 3000 (both included).
+
+# 2. Write a Python program to convert temperatures to and from Celsius and Fahrenheit.
+# [ Formula : c/5 = f-32/9 [ where c = temperature in celsius and f = temperature in fahrenheit ]
+# Expected Output :
+# 60°C is 140 in Fahrenheit
+# 45°F is 7 in Celsius
+
