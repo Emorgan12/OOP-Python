8b307486807f0c624dc37ed2bb215e9ca124a061
Syed Murad <murad6000@yahoo.com>
2023-02-21 08:30:11 +0000

    commit

--- S/ex02.py
+++ S/ex02.py
@@ -40,10 +40,12 @@ print("The temperature in", o_convention, "is", result, "degrees.")
 # 3. Write a Python program to guess a number between 1 and 9. 
 # Note : User is prompted to enter a guess. If the user guesses wrong then the prompt appears again until the guess is correct, on successful guess, user will get a "Well guessed!" message, and the program will exit.
 
-
-
-
-
+import random
+target_num, guess_num = random.randint(1, 10), 0
+while target_num != guess_num:
+    guess_num = int(input('Guess a number between 1 and 10 until you get it right : '))
+print('Well guessed!')
+20
 
 # 4. Write a Python program to construct the following pattern, using a nested for loop.
 
@@ -57,9 +59,25 @@ print("The temperature in", o_convention, "is", result, "degrees.")
 # * * 
 # *
 
+n=5;
+for i in range(n):
+    for j in range(i):
+        print ('* ', end="")
+    print('')
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print('* ', end="")
+    print('')
+
 
 # 5. Write a Python program that accepts a word from the user and reverses it.
 
+word = input("Input a word to reverse: ")
+
+for char in range(len(word) - 1, -1, -1):
+  print(word[char], end="")
+print("\n")
 
 # 6. Write a Python program to count the number of even and odd numbers in a series of numbers 
 # Sample numbers : numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) 
@@ -67,15 +85,34 @@ print("The temperature in", o_convention, "is", result, "degrees.")
 # Number of even numbers : 5
 # Number of odd numbers : 4
 
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9,987,999) # Declaring the tuple
+count_odd = 0
+count_even = 0
+for x in numbers:
+        if not x % 2:
+    	     count_even+=1
+        else:
+    	     count_odd+=1
+print("Number of even numbers :",count_even)
+print("Number of odd numbers :",count_odd)
 
 # 7. Write a Python program that prints each item and its corresponding type from the following list.
 # Sample List : datalist = [1452, 11.23, 1+2j, True, 'w3resource', (0, -1), [5, 12], {"class":'V', "section":'A'}]
 
+datalist = [1452, 11.23, 1+2j, True, 'w3resource', (0, -1), [5, 12],
+{"class":'V', "section":'A'}]
+for item in datalist:
+   print ("Type of ",item, " is ", type(item))
 
 # 8. Write a Python program that prints all the numbers from 0 to 6 except 3 and 6.
 # Note : Use 'continue' statement.
 # Expected Output : 0 1 2 4 5
 
+for x in range(6):
+    if (x == 3 or x==6):
+        continue
+    print(x,end=' ')
+print("\n")
 
 # 9. Write a Python program to get the Fibonacci series between 0 and 50. 
 # Note : The Fibonacci Sequence is the series of numbers :
@@ -83,6 +120,11 @@ print("The temperature in", o_convention, "is", result, "degrees.")
 # Every next number is found by adding up the two numbers before it.
 # Expected Output : 1 1 2 3 5 8 13 21 34
 
+x,y=0,1
+
+while y<50:
+    print(y)
+    x,y = y,x+y
 
 # 10. Write a Python program that iterates the integers from 1 to 50. For multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz". For numbers that are multiples of three and five, print "FizzBuzz".
 # Sample Output :
@@ -93,6 +135,17 @@ print("The temperature in", o_convention, "is", result, "degrees.")
 # 4
 # buzz
 
+for fizzbuzz in range(51):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        continue
+    print(fizzbuzz)
 
 # 11. Write a Python program that takes two digits m (row) and n (column) as input and generates a two-dimensional array. The element value in the i-th row and j-th column of the array should be i*j. 
 # Note :
@@ -102,13 +155,44 @@ print("The temperature in", o_convention, "is", result, "degrees.")
 # Test Data : Rows = 3, Columns = 4
 # Expected Result : [[0, 0, 0, 0], [0, 1, 2, 3], [0, 2, 4, 6]]
 
+row_num = int(input("Input number of rows: "))
+col_num = int(input("Input number of columns: "))
+multi_list = [[0 for col in range(col_num)] for row in range(row_num)]
+
+for row in range(row_num):
+    for col in range(col_num):
+        multi_list[row][col]= row*col
+
+print(multi_list)
 
 # 12. Write a Python program that accepts a sequence of lines (blank line to terminate) as input and prints the lines as output (all characters in lower case). 
+
+lines = []
+while True:
+    l = input()
+    if l:
+        lines.append(l.upper())
+    else:
+        break;
+
+for l in lines:
+    print(l)
 
 # 13. Write a Python program that accepts a sequence of comma separated 4 digit binary numbers as its input. The program will print the numbers that are divisible by 5 in a comma separated sequence. 
 # Sample Data : 0100,0011,1010,1001,1100,1001
 # Expected Output : 1010
 
+s = input("Input a string")
+d=l=0
+for c in s:
+    if c.isdigit():
+        d=d+1
+    elif c.isalpha():
+        l=l+1
+    else:
+        pass
+print("Letters", l)
+print("Digits", d)
 
 # 14. Write a Python program that accepts a string and calculates the number of digits and letters. 
 # Sample Data : Python 3.2
@@ -116,6 +200,29 @@ print("The temperature in", o_convention, "is", result, "degrees.")
 # Letters 6
 # Digits 2
 
+import re
+p= input("Input your password")
+x = True
+while x:  
+    if (len(p)<6 or len(p)>12):
+        break
+    elif not re.search("[a-z]",p):
+        break
+    elif not re.search("[0-9]",p):
+        break
+    elif not re.search("[A-Z]",p):
+        break
+    elif not re.search("[$#@]",p):
+        break
+    elif re.search("\s",p):
+        break
+    else:
+        print("Valid Password")
+        x=False
+        break
+
+if x:
+    print("Not a Valid Password")
 
 # 15. Write a Python program to check the validity of passwords input by users. 
 # Validation :
@@ -126,9 +233,21 @@ print("The temperature in", o_convention, "is", result, "degrees.")
 # Minimum length 6 characters.
 # Maximum length 16 characters.
 
+items = []
+for i in range(100, 401):
+    s = str(i)
+    if (int(s[0])%2==0) and (int(s[1])%2==0) and (int(s[2])%2==0):
+        items.append(s)
+print( ",".join(items))
 
 # 16. Write a Python program to find numbers between 100 and 400 (both included) where each digit of a number is an even number. The numbers obtained should be printed in a comma-separated sequence.
 
+items = []
+for i in range(100, 401):
+    s = str(i)
+    if (int(s[0])%2==0) and (int(s[1])%2==0) and (int(s[2])%2==0):
+        items.append(s)
+print( ",".join(items))
 
 # 17. Write a Python program to print the alphabet pattern 'A'. 
 # Expected Output:
@@ -140,3 +259,13 @@ print("The temperature in", o_convention, "is", result, "degrees.")
 #  *   *                                                                  
 #  *   *                                                                  
 #  *   *
+
+result_str="";    
+for row in range(0,7):    
+    for column in range(0,7):     
+        if (((column == 1 or column == 5) and row != 0) or ((row == 0 or row == 3) and (column > 1 and column < 5))):    
+            result_str=result_str+"*"    
+        else:      
+            result_str=result_str+" "    
+    result_str=result_str+"\n"    
+print(result_str);
