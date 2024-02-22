afd0f12192de7b4bbdac37a9b6b58859eb16eb26
Syed Murad <murad6000@yahoo.com>
2024-02-22 10:43:21 +0000

    commit

--- Pandas/data.py
+++ Pandas/data.py
@@ -4,6 +4,8 @@ df = pd.read_csv('C:/Users/ITC-LAB-T/Desktop/OOP-Python/Pandas/data.csv')
 
 # print(df) 
 
+print(df.to_string()) 
+
 #print(pd.options.display.max_rows) 
 
 pd.options.display.max_rows = 9999
