17a32052914d462353d95f134378e3c462f67866
Syed Murad <murad6000@yahoo.com>
2024-02-22 10:23:21 +0000

    commit

--- Pandas/data.py
+++ Pandas/data.py
@@ -2,9 +2,10 @@ import pandas as pd
 
 df = pd.read_csv('C:/Users/ITC-LAB-T/Desktop/OOP-Python/Pandas/data.csv')
 
-print(df) 
+# print(df) 
 
-print(pd.options.display.max_rows) 
+#print(pd.options.display.max_rows) 
 
 pd.options.display.max_rows = 9999
 
+#pd.options.display.max_rows = 10
